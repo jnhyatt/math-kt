40b3e77c0fe090aba8b8e8fe24360165c755770c
Josh Hyatt <joshua.n.hyatt@gmail.com>
2023-11-27 15:09:17 -0700

    swear words

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
+//    `maven-publish`
     id("com.android.application") version "8.0.0" apply false
     id("com.android.library") version "8.0.0" apply false
     kotlin("android") version "1.9.20" apply false
